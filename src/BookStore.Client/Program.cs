a8951645709eaf485a56f10791fa9cfdf17f8b7e
Junustin <kritapaswinichayakul@gmail.com>
2026-08-25 17:04:16 +0700

    Fix client program.cs

--- src/BookStore.Client/Program.cs
+++ src/BookStore.Client/Program.cs
@@ -11,7 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5160") });
 
 builder.Services.AddTransient<JwtHandler>();
-uilder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+builder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<JwtHandler>();
 
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ServerAPI"));
